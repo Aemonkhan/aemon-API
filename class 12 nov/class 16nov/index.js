8b75537974642f819df137a7f25d635b60dd7031
Aemonkhan <ameonkhan789@gmail.com>
2020-11-17 12:57:22 +0500

    newWorking

--- class 12 nov/class 16nov/index.js
+++ class 12 nov/class 16nov/index.js
@@ -21,25 +21,57 @@
 //     return c
 // }
 // console.log(sum(2))
-var arr = [2, 4, 6, 8];
-const newArr = arr.map(
-    (item) => {
-        return item * item
-    }
-)
-console.log(newArr)
-var arr1 = [4, 9];
-const newArr1 = arr1.map(
-    (no) => {
-        return Math.sqrt(no)
-    }
-)
-console.log(newArr1)
-var str = ['aemon', 'habiba', 'kulsum'];
-const newOne = str.map(
-    (value, i) => {
+// var arr = [2, 4, 6, 8];
+// const newArr = arr.map(
+//     (item) => {
+//         return item * item
+//     }
+// )
+// console.log(newArr)
+// var arr1 = [4, 9];
+// const newArr1 = arr1.map(
+//     (no) => {
+//         return Math.sqrt(no)
+//     }
+// )
+// console.log(newArr1)
+// var str = ['aemon', 'habiba', 'kulsum'];
+// const newOne = str.map(
+//     (value, i) => {
 
-        return `${i} + ${value}`;
-    }
-)
-console.log(newOne)
+//         return `${i} + ${value}`;
+//     }
+// )
+// console.log(newOne)
+var teachers = ['faiza', 'ali', 'zia', 'inzamam']
+var students = ['kulsoom', 'aiman', 'komal', 'tooba']
+var ages = [18, 20, 16, 21];
+// var sub = [200, 100, 50, 20]
+// var toFind = 'aiman'
+// var a = students.find(
+//     (item, index) => {
+//         return item == toFind
+//     }
+// )
+// console.log(a)
+// var fine = 'aiman'
+// var b = students.includes('aiman')
+// console.log(b)
+// var c = ages.filter(
+//     (item, index) => {
+//         return item >= 18
+//     }
+// )
+// console.log(c)
+// var newArr = 'faiza'
+// var a = teachers.includes('faiza'
+// )
+// console.log(a)
+// if (a == true) {
+//     const b = students.map(
+//         (item, i) => {
+//             return item + ' ' + 'will have the class'
+//         }
+//     )
+//     console.log(b)
+// }
